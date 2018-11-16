d0c43b3d3101b122f5f460f9d664395c97eb15bb
Exclamation-mark <1694392889@qq.com>
2018-11-16 14:57:54 +0800

    添加相应component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [/*声明模块中有什么东西  只能声明 组件  指令 管道*/
-    AppComponent
+    AppComponent, NavbarComponent, FooterComponent, SearchComponent, CarouselComponent, ProductComponent, StarsComponent
   ],
   imports: [/*声明该模块所依赖的模块*/
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+/*导入方法*/
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 /*整个项目的入口，Angular通过这个文件来启动项目*/
 if (environment.production) {
+  /*如果是生产模式  就关闭开发者模式*/
   enableProdMode();
 }
-
+/*程序的起点，整个程序就是从这里运行的，AppModule指向的是
+/app/app.module，也就是说程序启动时会去加载/app/app.module这个文件*/
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
